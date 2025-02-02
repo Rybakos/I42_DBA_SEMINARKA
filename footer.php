a8d506158f4f7a3892214b131ebd537d36782d4a
Rybak <106737812+Rybakos@users.noreply.github.com>
2025-02-02 14:16:45 +0100

    edge
    edge

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Autor: Kárský Filip</p>
+    <p>Dnes je <?php echo date("l d/m/Y"); ?>.</p>
+    <p>Čas poslední návštěvy: <?php echo date("H") . " hodin " . date("i") . " minut " . date("s") . " sekund"; ?>.</p>
+</footer>
