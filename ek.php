7e430829f85bdeb275bc97c95c9005b785329997
Rybak <106737812+Rybakos@users.noreply.github.com>
2025-02-03 18:32:51 +0100

    sigma
    sigma

---
+++ ek.php
@@ -0,0 +1,76 @@
+<div class="ek">
+    <div class="ekuv">
+            <h2>💸Úvod</h2>
+            <p>Thajsko má jednu z nejsilnějších ekonomik jihovýchodní Asie, která je založená na exportu, průmyslu a turistice.</p>
+        </div>
+
+        <div class="eksekt">
+            <h2>🏦Hlavní sektory ekonomiky</h2>
+            <ol>
+                <li class="li1">🏭Průmysl
+                    <ul>
+                        <li class="li2">Automobilový průmysl</li>
+                        <li class="li2">Elektronika</li>
+                    </ul>
+                </li>
+                <li class="li1">☘️Zemědělství
+                    <ul>
+                        <li class="li2">Rýže</li>
+                        <li class="li2">Kaučuk</li>
+                        <li class="li2">Mořské plody</li>
+                    </ul>
+                </li>
+                <li class="li1">🥾Turismus
+                    <ul>
+                        <li class="li2">Kulturní turismus</li>
+                        <li class="li2">Přírodní turismus</li>
+                        <li class="li2">Zdravotní turismus</li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
+
+        <div class="loskontaktos">
+            <h2>📩Kontakt</h2>
+            <p>Pokud máte dotazy, 📨<a href="mailto:info@thaiskoekonomika.cz">kontaktujte nás</a>.</p>
+        </div>
+
+        <div class="ektable">
+            <h2>🥬Hospodářské ukazatele</h2>
+            <div class="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th rowspan="2">Rok</th>
+                            <th colspan="2">HDP (miliardy USD)</th>
+                        </tr>
+                        <tr>
+                            <th>Celkové</th>
+                            <th>Na obyvatele</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2020</td>
+                            <td>501</td>
+                            <td>7,200</td>
+                        </tr>
+                        <tr>
+                            <td>2021</td>
+                            <td>515</td>
+                            <td>7,400</td>
+                        </tr>
+                        <tr>
+                            <td>2022</td>
+                            <td>530</td>
+                            <td>7,600</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="jedemebomby">
+            <a href="#">Zpět na začátek</a>
+        </div>
+</div>
--- zaj.php
+++ zaj.php
@@ -1,17 +1,35 @@
-<div class="zaj">
+<div id="zaj">
     <div class="zajimavost">
         <h2>🐘 Thajsko a sloni</h2>
         <p>Sloni jsou považováni za posvátná zvířata a mají velký význam v thajské kultuře.</p>
+        <img src="./img/slonos.jpg" alt="Slon">
     </div>
 
     <div class="zajimavost">
         <h2>🍜 Pouliční jídlo</h2>
         <p>Thajsko je rájem pouličního jídla. Vyzkoušej Pad Thai, Tom Yum nebo Mango Sticky Rice!</p>
+        <img src="./img/streetfoodjpg.jpg" alt="Profík v oboru pouliční jídlo">
     </div>
 
     <div class="zajimavost">
         <h2>🏛️ Nejdelší název města</h2>
         <p>Bangkok má oficiálně nejdelší název města na světě!</p>
+        <img src="./img/bangkoknazev.jpg" alt="Celej název Bangkoku">
+    </div>
+    <div class="zajimavost">
+        <h2>🐅 Thajští tygři</h2>
+        <p>Thajsko je domovem thajského tygra, jednoho z nejohroženějších druhů tygra na světě. Tento majestátní predátor je symbolem síly a odvahy v thajské kultuře.</p>
+        <img src="./img/tiger.jpg" alt="Tiger typu hodně stylovej">
+    </div>
+    <div class="zajimavost">
+        <h2>🦋 Biodiverzita</h2>
+        <p>Thajsko je jedním z nejvíce biodiverzních regionů na světě. Má více než 10 000 rostlinných druhů a 1 000 živočišných druhů, včetně mnoha ohrožených druhů.</p>
+        <img src="./img/biodiversita.jpg" alt="Obrázek Biodiversity">
+    </div>
+    <div class="zajimavost">
+        <h2>👩‍🦰 Co je to "Kathoey"?</h2>
+        <p>Termín Kathoey v thajštině označuje osoby, které se identifikují jako transgender ženy nebo jako mužské-to-ženské transsexuálky. V angličtině se často používá termín ladyboy. V thajské kultuře jsou kathoey považováni za třetí pohlaví a mají dlouhou historickou a kulturní tradici.</p>
+        <img src="./img/ladyboy.jpg" alt="Překvapení v Thailandu">
     </div>
 
     <div class="zajimavost">
